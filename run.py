229dc7cdf98c09e8242fa4b09ce324cbe60ce32d
minhanh3110 <lehasong@gmail.com>
2025-05-31 09:48:35 +0700

    Update deployment configuration

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
-from app import create_app, db
-
-app = create_app()
+from app import app
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True) 
+    app.run(host='0.0.0.0', port=10000) 
